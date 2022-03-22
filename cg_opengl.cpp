6777e70584e15d173dca6c8978d4b63edf7a29fb
Bluevery818 <bluevery818@gmail.com>
2022-03-22 12:48:38 +0900

    3.22 display control

--- cg_opengl.cpp
+++ cg_opengl.cpp
@@ -1,31 +1,58 @@
 ﻿
-//날 짜 : 2022.03.15
+//날 짜 : 2022.03.22
 //작성자 : 강대한
-//주 제 : 콜백 관련 기본 소스
+//주 제 : 콜백 관련 함수
 #include <stdio.h>
 #include <glut.h> //운영체제 연결
 #include <glu.h> //그림 그리기
 #include <gl.h> //렌더링
+#include <math.h>
+#define GL_PI 3.1415f
 int BuSt = 0;
 double r=0, g=0, b=0;
+
 void MyDisplay() {
+	int iPivot = 1;
+	GLfloat x, y, z, angle;
 	glClear(GL_COLOR_BUFFER_BIT); //버퍼 초기화
-	glColor3f(r, g, b); // 도형 색깔
-	glBegin(GL_POLYGON); // 점들을 면으로
-		glVertex3f(0.0, -0.45, 0.0); // 3: 인자 3개(3차원), f: 실수
-		glVertex3f(0.55, -0.75, 0.0);
-		glVertex3f(0.4, -0.25, 0.0);
-		glVertex3f(0.75, 0.0, 0.0);
-		glVertex3f(0.25, 0.2, 0.0);
-		glVertex3f(0.0, 0.65, 0.0);
-		glVertex3f(-0.25, 0.2, 0.0);
-		glVertex3f(-0.75, 0.0, 0.0);
-		glVertex3f(-0.4, -0.25, 0.0);
-		glVertex3f(-0.55, -0.75, 0.0);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	////glPointSize(8.0f); // 점의 크기
+	////glBegin(GL_POINTS); // 점들을 면으로
+	////
+	////z = -0.8f;
+	////for (angle = 0.0f; angle <= (2.0f * GL_PI) * 3.0f; angle += 0.1f) {
+	////	x = sin(angle)/2;
+	////	y = cos(angle)/2;
+	////	glVertex3f(x, y, z);
+	////	z += 0.02f;
+	////}
+	////glEnd();
+	////glLineWidth(5.0f);
+	////glBegin(GL_LINE_STRIP); // //GL_LINES  //GL_LINE_LOOP
+	////z = 0.0f;
+	////for (angle = 0.0f; angle <= GL_PI*2*4; angle += 0.1f) {
+	////	x = sin(angle) / 1.5;
+	////	y = cos(angle) / 1.5;
+	////	glVertex3f(x, y, z);
+	////	z += 0.05f;
+	//	x = (sin(angle + GL_PI)) / 1.5;
+	//	y = (cos(angle + GL_PI)) / 1.5;
+	//	glVertex3f(x, y, z);
+	//}
+	glShadeModel(GL_FLAT); //GL_SMOOTH
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex3f(0.0f, 0.0f, 1.0f);
+	for (angle = 0.0f; angle < (4.0f * GL_PI); angle += (GL_PI / 4.0f)) {
+		x = sin(angle)/1.5f; y = cos(angle)/1.5;
+		if ((iPivot % 2) == 0)
+			glColor3f(0.0f, 1.0f, 0.0f);
+		else
+			glColor3f(1.0f, 0.0f, 0.0f);
+		iPivot++;
+		glVertex2f(x, y);
+	}
 	glEnd();
-	glFlush(); // 버퍼 내용 비우기(출력)
-
-
+	glutSwapBuffers();
 }
 void MyReshape(int NewWidth, int NewHeight) { } // 균형 유지 위한 변형값을 인자로 받음 (자동)
 
@@ -57,7 +84,7 @@ void MyMouseClick(GLint Button, GLint State, GLint X, GLint Y) {
 		b = 0.0;
 		BuSt = 0;
 	}
-
+	glutPostRedisplay();// 디스플레이 강제 호출 *디스플레이 콜백함수만 가능
 } // 왼 or 오른쪽, 누름 or 해제 etc, 마우스 위치
 
 void MyMouseMove(GLint X, GLint Y) {
@@ -79,11 +106,13 @@ void MenuFunc() {}
 // argc : 인자 수 argv : 인자
 int main(int argc, char** argv) {
 	glutInit(&argc, argv); //초기화 초기설정
-	glutInitDisplayMode(GLUT_RGB); // 화면 설정
+	//glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE); // 화면 설정 single=glFlush()
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH); // double=glutSwapBuffers()
+
 	glutInitWindowSize(500, 500); // 화면 크기
 	glutInitWindowPosition(100, 200); // 초기 화면 위치(시작점 좌측 상단)
 	glutCreateWindow("제목"); // 그래픽창 생성
-	glClearColor(0.5, 0.5, 0.5, 1.0); // 색 초기화 r, g, b, 투명도 256:1 *바탕색 1,1,1=흰색 0, 0, 0 = 검정색
+	glClearColor(1, 1, 1, 1.0); // 색 초기화 r, g, b, 투명도 256:1 *바탕색 1,1,1=흰색 0, 0, 0 = 검정색
 
 	glutDisplayFunc(MyDisplay); // 콜백함수, 함수를 인자로 받고 리턴값 받음, 함수 호출 후 대기 안 함(일반 함수는 끝날 때까지 대기)
 
@@ -92,8 +121,8 @@ int main(int argc, char** argv) {
 	
 	/*glutKeyboardFunc(MyKeyboard);
 	glutSpecialFunc(MySpecial);*/
-	glutMouseFunc(MyMouseClick);
-	glutMotionFunc(MyMouseMove);
+	/*glutMouseFunc(MyMouseClick);
+	glutMotionFunc(MyMouseMove);*/
 	//glutIdleFunc(MyIdle); ex)idle: cpu 쉬는 시간에 작동되는 것 
 	//glutTimerFunc(40, MyTimer, 1); 
 	// end 콜백함수 등록
