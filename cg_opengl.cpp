e24b5a98a1df7423c3dd60398791441d8bf49a05
Bluevery8 <bluevery818@gmail.com>
2022-05-24 12:49:22 +0900

    gluLookAt

--- cg_opengl.cpp
+++ cg_opengl.cpp
@@ -4,38 +4,17 @@
 
 
 void MyDis() {
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	glColor3f(1.0f, 0.0f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glColor3f(0.5, 0.5, 0.5);
+	gluLookAt(0.0,0.5,0.0, 0.0,0.0,0.0, 0.0,90.0,0.0);
 	glutWireTeapot(0.5);
-	glBegin(GL_LINES);
-		glVertex3f(0, -5.0, 0.0); glVertex3f(0, 5.0, 0.0);
-		glVertex3f(-5.0, 0.0, 0.0); glVertex3f(5.0, 0.0, 0.0);
-	glEnd();
-
-	glRotatef(180.0f, 1.0f, 1.0f, 0.0);
-	glTranslatef(3.0, 1.0, 0.0);
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glutWireTeapot(0.5);
-	glBegin(GL_LINES);
-	glVertex3f(0, -5.0, 0.0); glVertex3f(0, 5.0, 0.0);
-	glVertex3f(-5.0, 0.0, 0.0); glVertex3f(5.0, 0.0, 0.0);
-	glEnd();
+	glFlush();
 
 	
 	glFlush();
 }
 void MyRes(int NW, int NH) {
-	GLfloat nRange = 5.0f;
-	if (NH == 0)
-		NH = 1;
-	glViewport(0, 0, NW, NH);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	if (NW <= NH)
-		glOrtho(-nRange, nRange, -nRange * NH / NW, nRange * NH / NW, -nRange, nRange);
-	else
-		glOrtho(-nRange * NW / NH, nRange * NW / NH, -nRange, nRange, -nRange, nRange);
+	
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
@@ -75,14 +54,14 @@ int main(int argc, char** argv) {
 	glClearColor(1.0, 1.0, 1.0, 1.0);
 
 	glutDisplayFunc(MyDis);
-	glutReshapeFunc(MyRes);
-	glutKeyboardFunc(MyKey);
+	//glutReshapeFunc(MyRes);
+	/*glutKeyboardFunc(MyKey);
 	glutSpecialFunc(MySpe);
 	glutMouseFunc(MyClick);
 	glutMotionFunc(MyMove);
 	glutIdleFunc(MyIdle);
-	glutTimerFunc(40,MyTim,1);
-	MenuFunc();
+	glutTimerFunc(40,MyTim,1);*/
+	
 
 	glutMainLoop();
 	return 0;
