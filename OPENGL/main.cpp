ccf5a91a451249a9e55845e64e0d34479aaa3c04
Bluevery8 <bluevery818@gmail.com>
2022-05-24 16:59:51 +0900

    Initial Commit

---
+++ OPENGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OPENGL
+//
+//  Created by Bluevery8 on 2022/05/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
